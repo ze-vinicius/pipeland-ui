b3db9dabbf36b2c5b9113cd8c8206e308d7620cc
saraivavini <josevsaraiva@gmail.com>
2021-10-05 03:57:21 -0300

    refactor: add correct type to theme object

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,8 +1,9 @@
+import { Theme } from 'styled-system';
 import { colors } from './colors';
 import { space } from './space';
 import { fontSizes } from './fontSizes';
 
-export const theme = {
+export const theme: Theme = {
   colors,
   fontSizes,
   space,
