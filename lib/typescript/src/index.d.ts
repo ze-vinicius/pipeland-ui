8afa4ec30fb6c43f3be81b501363ef1d825f38a6
saraivavini <josevsaraiva@gmail.com>
2021-10-05 04:06:36 -0300

    chore: remove lib from local files

--- lib/typescript/src/index.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { ThemeType } from './theme';
-export * from './components';
-export * from './theme';
-export interface ThemeProviderProps {
-    theme?: ThemeType;
-}
-declare const ThemeProvider: React.FC<ThemeProviderProps>;
-export { ThemeProvider };
